9cce37329e2175182f8e2bca5776f57a5c81000a
Yashraj-singh785 <yashrajsingh7851@gmail.com>
2025-01-19 02:30:21 +0530

    Create yash.js

---
+++ yash.js
@@ -0,0 +1,18 @@
+const createStoreBtn = document.getElementById('createStore');
+
+createStoreBtn.addEventListener('click', () => {
+    // Handle the "Create Your Store" button click 
+    // (e.g., navigate to a new page, display a message, etc.)
+
+    alert('Redirecting to store creation page!'); 
+});
+
+const addToCartBtn = document.getElementById('addToCart');
+
+addToCartBtn.addEventListener('click', () => {
+    // Handle the "Add to Cart" button click 
+    // (e.g., display a success message, 
+    // update a cart counter, etc.)
+
+    alert('Product added to cart!'); 
+});
